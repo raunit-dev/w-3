a8db0b3e188f284f5ebf4a59b8edb369cb27b3d4
raunit-dev <raunitjaiswal510@gmail.com>
2025-02-28 18:02:53 +0530

    today commit\

--- crypto.js
+++ crypto.js
@@ -1,24 +1,25 @@
 import { createHash } from 'crypto';
 
-function findHashWithPrefix(prefix) {
-    let number = 0;
-    while (true) {
-        const input = "harkirat => raman|100"+number.toString();
-        const hash = createHash('sha256').update(input).digest('hex');
-        if (hash.startsWith(prefix)) {
-            return { input: number.toString(), hash };
-        }
-        number++;
-}
-}
-const result = findHashWithPrefix('00000');
-console.log('Input:', result.input);
-console.log('Hash:', result.hash);
+// function findHashWithPrefix(prefix) {
+//     let number = 0;
+//     while (true) {
+//         const input = "harkirat => raman|100"+number.toString();
+//         const hash = createHash('sha256').update(input).digest('hex');
+//         if (hash.startsWith(prefix)) {
+//             return { input: number.toString(), hash };
+//         }
+//         number++;
+// }
+// }
+// const result = findHashWithPrefix('00000');
+// console.log('Input:', result.input);
+// console.log('Hash:', result.hash);
+
 
 function FIND(prefix) {
    let number = 0;
    while(true) {
-         const input = "I MET A GIRL"+number.toString();
+         const input = "Raunit Jaiswal"+number.toString();
          const hash = createHash('sha256').update(input).digest('hex');
          if(hash.startsWith(prefix)) {
               return { input: number.toString(), hash };
@@ -26,6 +27,6 @@ function FIND(prefix) {
          number++;
    }
 }
-const value = FIND('RAUNIT');
+const value = FIND('000000');
 console.log(value.input);
 console.log(value.hash);
